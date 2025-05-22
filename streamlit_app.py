1fb288da9adf1c0418db585b1bc3b1fe8b594032
kirari-5261124 <kt07111227kt@icloud.com>
2025-05-22 08:00:25 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ st.title('世界の言葉')
 st.write('こんにちは')
 user_name = st.text_input('名前を入力してください')
 st.header('あなたの名前は' + str(user_name) + 'です')
-h = st.number_input('身長を入力してください(m)')
-w = st.number_input('体重を入力してください')
+h = st.number_input('身長を入力してください(m)',value = 1.6)
+w = st.number_input('体重を入力してください', value = 50)
 
 bmi = w/(h**2)
 
